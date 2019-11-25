d69ee4efaaf9b730ba4a0b399728c215510c0806
ie80s <ie80s@163.com>
2019-11-25 10:56:52 +0800

    1

--- src/components/module/condition.vue
+++ src/components/module/condition.vue
@@ -1,16 +1,92 @@
 <template>
     <div id="Condition">
         <div class="Condition-1">
-
+            <el-row :gutter="24">
+                <el-col :span="12">
+                    <div class="grid-content bg-purple">
+                        <span class="cond-left">时间：{{times}}</span>
+                        <span class="cond-left">详情：{{details}}</span>
+                        <div class="demo-image__preview">
+                            <el-image 
+                                style="width: 100px; height: 100px"
+                                :src="url" 
+                                :preview-src-list="srcList">
+                            </el-image>
+                        </div>
+                    </div>
+                </el-col>
+                <el-col :span="12">
+                    <div class="grid-content bg-purple">
+                        <span class="cond-right">状态：{{state}}</span>
+                    </div>
+                </el-col>
+            </el-row>           
         </div>
         <div class="Condition-2">
-
+            <el-row :gutter="24">
+                <el-col :span="12">
+                    <div class="grid-content bg-purple">
+                        <span class="cond-left">时间：{{times}}</span>
+                        <span class="cond-left">详情：{{details}}</span>
+                        <div class="demo-image__preview">
+                            <el-image 
+                                style="width: 100px; height: 100px"
+                                :src="url" 
+                                :preview-src-list="srcList">
+                            </el-image>
+                        </div>
+                    </div>
+                </el-col>
+                <el-col :span="12">
+                    <div class="grid-content bg-purple">
+                        <span class="cond-right">状态：{{state}}</span>
+                    </div>
+                </el-col>
+            </el-row>
         </div>
         <div class="Condition-3">
-
+            <el-row :gutter="24">
+                <el-col :span="12">
+                    <div class="grid-content bg-purple">
+                        <span class="cond-left">时间：{{times}}</span>
+                        <span class="cond-left">详情：{{details}}</span>
+                        <div class="demo-image__preview">
+                            <el-image 
+                                style="width: 100px; height: 100px"
+                                :src="url" 
+                                :preview-src-list="srcList">
+                            </el-image>
+                        </div>
+                    </div>
+                </el-col>
+                <el-col :span="12">
+                    <div class="grid-content bg-purple">
+                        <span class="cond-right">状态：{{state}}</span>
+                    </div>
+                </el-col>
+            </el-row>
         </div>
         <div class="Condition-4">
-
+            <el-row :gutter="24">
+                <el-col :span="12">
+                    <div class="grid-content bg-purple">
+                        <span class="cond-left">时间：{{times}}</span>
+                        <span class="cond-left">详情：{{details}}</span>
+                        <div class="demo-image__preview">
+                            <el-image 
+                                style="width: 100px; height: 100px"
+                                :src="url" 
+                                :preview-src-list="srcList">
+                            </el-image>
+                        </div>
+                    </div>
+                </el-col>
+                <el-col :span="12">
+                    <div class="grid-content bg-purple">
+                        <span class="cond-right">状态：{{state}}</span>
+                    </div>
+                </el-col>
+            </el-row>
         </div>
         <div class="block">
             <span class="demonstration"></span>
@@ -31,7 +107,8 @@
 export default {
     data(){
         return{      
-            currentPage3: 1,  
+            currentPage3: 1, 
+            url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
         }
 
     },
@@ -49,6 +126,18 @@ export default {
 
 
 <style scoped lang="scss">
+
+.cond-left{
+    margin-right: 90%;
+}
+
+.demo-image__preview{
+    margin-right: 90%;
+}
+
+.cond-right{
+    margin-left: 80%;
+}
 
 #Condition{
     width: 100%;
@@ -92,5 +181,25 @@ export default {
     }
 }
 
+
+
+.el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
 
 </style>
