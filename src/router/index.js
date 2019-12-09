b21acdf3e886a0d58315a3a2cb8776d0c95f37cd
ie80s <ie80s@163.com>
2019-12-09 10:56:41 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import logoin from '@/components/deng/logoin'
-import index from '@/components/index'
 import Logon from  '@/components/deng/Logon'
 import message from '@/components/message'
 import myrepair from '@/components/myrepair'
@@ -10,12 +9,7 @@ import myrepair from '@/components/myrepair'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'index',
-      component: index
-    },
+  routes: [  
     {
       path:'/deng/logoin',
       name:'logoin',
